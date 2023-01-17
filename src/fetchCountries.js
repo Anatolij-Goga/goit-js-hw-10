a368c41951c2b301978ddf86d9ad588851aefe5a
Anatolij Goga <anzefal@gmail.com>
2023-01-17 22:59:22 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,13 +2,12 @@ export { fetchCountries };
 
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-const fetchCountries = name => {
-  return fetch(
+const fetchCountries = async name => {
+  const response = await fetch(
     `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  );
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
 };
